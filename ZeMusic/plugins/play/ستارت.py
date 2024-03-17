fd4fe4d35ea6f18dc295e65e294ef980353f9b21
qwert200 <159542184+qwert200@users.noreply.github.com>
2024-03-17 04:04:21 +0300

    Update ستارت.py

--- ZeMusic/plugins/play/ستارت.py
+++ ZeMusic/plugins/play/ستارت.py
@@ -4,7 +4,37 @@ from ZeMusic import app as Client
 from ZeMusic import app
 from pyrogram.types import InlineKeyboardButton
 import config
+import asyncio
+import random
+from strings.filters import command
 
+
+@app.on_message(
+    command(["/start"])
+    & filters.group
+    & filters.private 
+)
+async def iddd(client, message):
+    if message.chat.id in iddof:
+      return
+    usr = await client.get_chat(message.from_user.id)
+    name = usr.first_name
+    photo = await app.download_media(usr.photo.big_file_id)
+    await message.reply_photo(photo,       caption=f"""🤡""", 
+    reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(text=" English 🇺🇸 ", callback_data="english"), 
+            InlineKeyboardButton(text=" عربي 🇮🇶 ", callback_data="arbic")],
+        [
+            InlineKeyboardButton(
+                        " الـمطور ", user_id=config.OWNER_ID 
+                    )
+                ],
+            ]
+        ),
+        disable_web_page_preview=True,
+    )
 
 @Client.on_callback_query(filters.regex("arbic"))
 async def arbic(_, query: CallbackQuery):
